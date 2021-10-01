f0ef8f91db71ae965249435f9d6ac14b5e097be0
littlepaike <f335125303@163.com>
2021-10-01 16:17:33 +0800

    AidPaike练习scala的wordcount

---
+++ src/main/java/test/flink/fanfan/WordCount.scala
@@ -0,0 +1,30 @@
+package test.flink.fanfan;
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, createTypeInformation}
+import scala.concurrent.ExecutionContext
+
+/**
+ * @Author littlepaike
+ * @Date 2021/10/1 16:13
+ * @Description: test.flink.fanfan
+ * @version: 1.0
+ * */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 创建一个批处理执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputPath = "E:\\documents\\wc.txt"
+    val inputDataSet = env.readTextFile(inputPath)
+
+    // 分词、转换、求和
+    val wordCountDataSet = inputDataSet.flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    // 打印
+    wordCountDataSet.print()
+  }
+}
